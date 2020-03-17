802c612f12d9dc8ccf780cced3d04a0ef10caf48
Riley <rileyw13@protonmail.com>
2020-03-17 19:45:39 -0400

    added function declarations

---
+++ lib/tsp-ilp.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <boost/numeric/ublas/triangular.hpp>
+#include <boost/numeric/ublas/io.hpp>
+#include <glpk.h>
+
+using namespace std;
+namespace ublas = boost::numeric::ublas;
+
+typedef ublas::triangular_matrix<double, ublas::upper> weighted_ugraph;
+typedef ublas::triangular_matrix<double, ublas::upper> edge_selection;
+
+void tsp_matrix_to_ilp();
