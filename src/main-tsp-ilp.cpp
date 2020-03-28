2cdfa2634940913c272688126bc9760a764d6072
Riley <rileyw13@protonmail.com>
2020-03-28 15:15:51 -0400

    more prints for debugging adjusted vector indices to match everythign else

--- src/main-tsp-ilp.cpp
+++ src/main-tsp-ilp.cpp
@@ -13,17 +13,17 @@ int main(){
   // edge list representation of graph
   // one edge: {start_node, end_node, weight}
   // edges are assumed to be bidirectional
-  vector<vector<int>> g(e);
-  g[0] = {1, 2, 1};
-  g[1] = {1, 4, 2};
-  g[2] = {1, 5, 7};
+  vector<vector<int>> g(e + 1);
+  g[1] = {1, 2, 1};
+  g[2] = {1, 4, 2};
+  g[3] = {1, 5, 7};
 
-  g[3] = {2, 3, 6};
-  g[4] = {2, 4, 3};
+  g[4] = {2, 3, 6};
+  g[5] = {2, 4, 3};
 
-  g[5] = {3, 5, 4};
+  g[6] = {3, 5, 4};
 
-  g[6] = {4, 5, 10};
+  g[7] = {4, 5, 10};
 
   // in this graph, shortest tour is 3, 0, 1, 2, 4 with cost 13
   //r, c
@@ -39,10 +39,18 @@ int main(){
   // g(3, 4) = 10;
 
   /* declare c variables */
+  const size_t arr_size = 1000;
   glp_prob *tsp;
-  int ia[1+1000], ja[1+1000];
-  double ar[1+1000];
+  int ia[1+arr_size], ja[1+arr_size];
+  double ar[1+arr_size];
   size_t index = 1;
+
+  // make all arrays 0, will be useful later when we print things
+  for (size_t i = 0; i < 1 + arr_size; i++){
+    ia[i] = 0;
+    ja[i] = 0;
+    ar[i] = 0.0;
+  }
 
   /* create problem */
   tsp = glp_create_prob();
@@ -72,16 +80,14 @@ int main(){
     glp_set_row_name(tsp, k,
       ("ensuring connectedness for k = " + to_string(k)).c_str());
 
-    cout << "g size: " << g.size() << " \n";
-    for(size_t i = 0; i < g.size(); i++){
-      cout << "g[i] size: " << g[i].size() << " \n";
+    for(size_t i = 1; i < 1 + e; i++){
       if(g[i][0] == int(k) || g[i][1] == int(k)){
         // don't use the weights, just use 1 because we want x_ij to be summed
         // here, not the weight
-        ia[index] = k, ja[index] = i + 1, ar[index] = 1;
+        ia[index] = k, ja[index] = i, ar[index] = 1;
       }
       else{
-        ia[index] = k, ja[index] = i + 1, ar[index] = inf;
+        ia[index] = k, ja[index] = i, ar[index] = 0;
       }
       index++;
     }
@@ -95,14 +101,35 @@ int main(){
   double z = glp_get_obj_val(tsp);
   cout << "total weight of tour: " << z << "\n";
 
-  for(size_t i = 1; i < size_t(glp_get_num_cols(tsp)) + 1; i++){
-    double x = glp_get_col_prim(tsp, i);
-    cout << "column " << i << " has value " << x << "\n";
+  cout << "\nMatrix Entries:\n";
+  for(size_t i = 1; i < 1 + arr_size; i++){
+    if (ia[i] != 0 || ja[i] != 0){
+      cout << "matrix entry " << ia[i] << "," << ja[i] << " = " << ar[i] 
+           << "\n";
+    }
   }
 
+  cout << "\nVariables:\n";
+  for(size_t i = 1; i < size_t(glp_get_num_cols(tsp)) + 1; i++){
+    double x = glp_get_col_prim(tsp, i);
+    double y = glp_get_col_dual(tsp, i);
+    double s = glp_get_col_stat(tsp, i);
+    // I believe these are each of the variable values
+    cout << "column " << i << " has primal value " << x << "\n";
+    cout << "column " << i << " has dual value " << y << "\n";
+    cout << "column " << i << " has status " << s << "\n";
+  }
+
+  cout << "\nConstraints:\n";
   for(size_t i = 1; i < size_t(glp_get_num_rows(tsp)) + 1; i++){
     double x = glp_get_row_prim(tsp, i);
-    cout << "row " << i << " has value " << x << "\n";
+    double y = glp_get_row_dual(tsp, i);
+    double s = glp_get_row_stat(tsp, i);
+    double c = glp_get_obj_coef(tsp, i);
+    cout << "row " << i << " has primal value " << x << "\n";
+    cout << "row " << i << " has dual value " << y << "\n";
+    cout << "row " << i << " has status " << s << "\n";
+    cout << "row " << i << " has coefficient" << c << "\n";
   }
 
   /* housekeeping */
